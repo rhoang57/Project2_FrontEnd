2ce7e9f5d0be8145813db8c32d0e1a4b2c5c3380
rhoang57 <rhoang57@gmail.com>
2019-01-18 21:57:58 -0800

    initial commit

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,39 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("index", {
+        msg: "ANSR",
+        examples: dbExamples
+      });
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  // Load create-quiz page
+  app.get("/create-quiz", function(req, res) {
+    res.render("create-quiz");
+  });
+
+
+
+  // Load quizzes page, still needs function to .findAll or .findMany based on dropdown choice
+  app.get("/quiz-selection", function(req, res) {
+    res.render("quiz-selection");
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
