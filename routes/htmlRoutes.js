8e424e5f610875ecc87f510835bb8eed2953ee06
Carlos Villalta <CarlosFindley@protonmail.com>
2019-01-19 19:15:25 -0800

    added multiple style sheets per page, added html route key pairs to be able to support multiple style sheets, styled quiz-selection page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
+        style: "index.css",
         msg: "ANSR",
         examples: dbExamples
       });
@@ -22,21 +23,29 @@ module.exports = function(app) {
 
   // Load create-quiz page
   app.get("/create-quiz", function(req, res) {
-    res.render("create-quiz");
+    res.render("create-quiz", {
+      style: "create-quiz.css"
+    });
   });
 
   // Load quiz page
   app.get("/quiz", function(req, res) {
-    res.render("quiz");
+    res.render("quiz", {
+      style: "quiz.css"
+    });
   });
 
   // Load quizzes page, still needs function to .findAll or .findMany based on dropdown choice
   app.get("/quiz-selection", function(req, res) {
-    res.render("quiz-selection");
+    res.render("quiz-selection", {
+      style: "quiz-selection.css"
+    });
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.render("404", {
+      style: "404.css"
+    });
   });
 };
